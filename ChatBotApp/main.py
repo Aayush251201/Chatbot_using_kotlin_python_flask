1d58367795cbfdc1b9e2c96a223da865d49f3db9
Aayush251201 <65340499+Aayush251201@users.noreply.github.com>
2022-04-06 22:27:33 +0530

    Add files via upload

---
+++ ChatBotApp/main.py
@@ -0,0 +1,156 @@
+import json
+import pickle
+import random
+
+import nltk
+import numpy
+from keras.layers import Dropout
+from nltk.stem import LancasterStemmer
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.models import Sequential, model_from_json
+from tensorflow.keras.optimizers import SGD
+
+
+nltk.download('punkt')
+
+stemmer = LancasterStemmer()
+
+with open("intents.json") as file:
+    data = json.load(file)
+
+try:
+    with open("chatbot.pickle", "rb") as file:
+        words, labels, training, output = pickle.load(file)
+
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
+
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
+
+    labels = sorted(labels)
+
+    training = []
+    output = []
+
+    output_empty = [0 for _ in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+
+        wrds = [stemmer.stem(w.lower()) for w in doc]
+
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+
+        output_row = output_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+
+        training.append(bag)
+        output.append(output_row)
+
+    training = numpy.array(training)
+    output = numpy.array(output)
+
+    with open("chatbot.pickle", "wb") as file:
+        pickle.dump((words, labels, training, output), file)
+
+try:
+    yaml_file = open('chatbotmodel.yaml', 'r')
+    loaded_model_yaml = yaml_file.read()
+    yaml_file.close()
+    myChatModel = model_from_json(loaded_model_yaml)
+    myChatModel.load_weights("chatbotmodel.h5")
+    print("Loaded model from disk")
+
+except:
+    # Make our neural network
+    myChatModel = Sequential()
+    myChatModel.add(Dense(128, input_shape=[len(words)], activation='relu'))
+    myChatModel.add(Dropout(0.5))
+    myChatModel.add(Dense(64, activation='relu'))
+    myChatModel.add(Dropout(0.5))
+    myChatModel.add(Dense(len(labels), activation='softmax'))
+
+    # optimize the model
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    myChatModel.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+    # train the model
+    myChatModel.fit(training, output, epochs=200, batch_size=5, verbose=1)
+
+    # serialize model to yaml and save it to disk
+    model_yaml = myChatModel.to_json()
+    with open("chatbotmodel.yaml", "w") as y_file:
+        y_file.write(model_yaml)
+
+    # serialize weights to HDF5
+    myChatModel.save_weights("chatbotmodel.h5")
+    print("Saved model from disk")
+
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+
+    return numpy.array(bag)
+
+
+def chatWithBot(inputText):
+    currentText = bag_of_words(inputText, words)
+    currentTextArray = [currentText]
+    numpyCurrentText = numpy.array(currentTextArray)
+
+    if numpy.all((numpyCurrentText == 0)):
+        return "I didn't get that, try again"
+
+    result = myChatModel.predict(numpyCurrentText[0:1])
+    result_index = numpy.argmax(result)
+    tag = labels[result_index]
+
+    if result[0][result_index] > 0.7:
+        for tg in data["intents"]:
+            if tg['tag'] == tag:
+                responses = tg['responses']
+
+        return random.choice(responses)
+
+    else:
+        return "I didn't get that, try again"
+
+
+def chat():
+    print("Start talking with the chatbot (try quit to stop)")
+
+    while True:
+        inp = input("You: ")
+        if inp.lower() == "quit":
+            break
+
+        print(chatWithBot(inp))
+
+
+#chat()
